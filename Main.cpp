60820b43c6f25b18362610d876bcedd835aed26f
Waleed Ahmed <0100waleedahmed0100@gmail.com>
2025-01-22 16:05:28 +0500

    Display schedule of a particular train
    Users can now choose which train's schedule they want to see by entering
    its name. All the stations that the train is going to go to and the
    corresponding departure time will be shown

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <iomanip>
+#include <windows.h>
 using namespace std;
 
-void displaySchedules(int, char trains[], string departure[], int stops[], string schedules[4][10]);
+void separator();
+void displaySchedules(int, char trains[], string departure[], int stops[], string schedules[4][10][2]);
 
 int main()
 {
@@ -12,18 +14,50 @@ int main()
     int avalaibleBerths[n] = {40, 40, 40, 40};
     int stops[] = {7, 7, 9, 9}; // Number of places the trains are going to stop at as shown in the following array. Each entry corresponds to trains[i]
 
-    string schedules[4][10] = {
-        // Train A
-        {"Rawalpindi", "Lahore", "Khanewal", "Bahawalpur", "Rohri", "Hyderabad", "Karachi"},
+    string schedules[4][10][2] = {
+        // Train A: Rawalpindi to Karachi
+        {
+            {"Rawalpindi", "05:00"},
+            {"Lahore", "08:30"},
+            {"Khanewal", "12:30"},
+            {"Bahawalpur", "15:00"},
+            {"Rohri", "19:30"},
+            {"Hyderabad", "23:00"},
+            {"Karachi", "(Destination)"}},
 
-        // Train B
-        {"Karachi", "Hyderabad", "Rohri", "Bahawalpur", "Khanewal", "Lahore", "Rawalpindi"},
+        // Train B: Karachi to Rawalpindi
+        {
+            {"Karachi", "10:00"},
+            {"Hyderabad", "11:30"},
+            {"Rohri", "15:00"},
+            {"Bahawalpur", "19:30"},
+            {"Khanewal", "22:00"},
+            {"Lahore", "02:00"},
+            {"Rawalpindi", "(Destination)"}},
 
-        // Train C
-        {"Lahore", "Sahiwal", "Khanewal", "Bahawalpur", "Rahim Yar Khan", "Rohri", "Nawabshah", "Hyderabad", "Karachi"},
+        // Train C: Lahore to Karachi
+        {
+            {"Lahore", "16:00"},
+            {"Sahiwal", "18:00"},
+            {"Khanewal", "20:30"},
+            {"Bahawalpur", "22:30"},
+            {"Rahim Yar Khan", "01:00"},
+            {"Rohri", "04:30"},
+            {"Nawabshah", "07:00"},
+            {"Hyderabad", "09:00"},
+            {"Karachi", "(Destination)"}},
 
-        // Train D
-        {"Karachi", "Hyderabad", "Nawabshah", "Rohri", "Rahim Yar Khan", "Bahawalpur", "Khanewal", "Sahiwal", "Lahore"}};
+        // Train D: Karachi to Lahore
+        {
+            {"Karachi", "21:00"},
+            {"Hyderabad", "22:30"},
+            {"Nawabshah", "01:00"},
+            {"Rohri", "04:30"},
+            {"Rahim Yar Khan", "07:00"},
+            {"Bahawalpur", "10:00"},
+            {"Khanewal", "13:00"},
+            {"Sahiwal", "15:30"},
+            {"Lahore", "(Destination)"}}};
 
     int priceA[] = {0, 1000, 2000, 3000, 4000, 5000, 6000};
     int priceB[] = {0, 1000, 2000, 3000, 4000, 5000, 6000};
@@ -31,35 +65,56 @@ int main()
     int priceD[] = {0, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500};
 
     int choice;
-    cout << "=> Press 1 to view train schedules" << endl;
-    cout << "=> Press 2 to book tickets" << endl;
-    cout << "=> Press 3 to view your ticket" << endl;
-    cout << "=> Press 4 to exit" << endl;
-    cout << "Enter your choice: ";
-    cin >> choice;
-
-    if (choice == 1)
+    while (true)
     {
-        displaySchedules(n, trains, departure, stops, schedules);
+        cout << "=> Press 1 to view train schedules" << endl;
+        cout << "=> Press 2 to book tickets" << endl;
+        cout << "=> Press 3 to view your ticket" << endl;
+        cout << "=> Press 0 to exit" << endl
+             << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 0)
+        {
+            break;
+        }
+        if (choice == 1)
+        {
+            displaySchedules(n, trains, departure, stops, schedules);
+        }
     }
 
     return 0;
 }
-
-void displaySchedules(int n, char trains[], string departure[], int stops[], string schedules[4][10])
+void separator()
 {
-    cout << setw(15) << left << "Name" << setw(25) << left << "Departure Time" << setw(30) << left << "Schedule" << endl;
+    cout << endl
+         << "--------------------------------------------------------------------" << endl
+         << endl;
+}
+void displaySchedules(int n, char trains[], string departure[], int stops[], string schedules[4][10][2])
+{
+    char tname;
+    cout << "Enter name of the train (A, B, C, D): ";
+    cin >> tname;
+    tname = toupper(tname);
+
+    int tindex;
     for (int i = 0; i < n; i++)
     {
-        cout << setw(5) << left << "Train " << setw(5) << left << trains[i] << setw(4) << "" << setw(25) << left << departure[i];
-        for (int j = 0; j < stops[i]; j++)
+        if (trains[i] == tname)
         {
-            cout << schedules[i][j];
-            if (j != stops[i] - 1)
-            {
-                cout << " => ";
-            }
+            tindex = i;
+            break;
         }
-        cout << endl;
     }
+    separator();
+    cout << setw(20) << left << "Station" << setw(20) << left << "Departure Time" << endl;
+    for (int i = 0; i < stops[tindex]; i++)
+    {
+        cout << setw(20) << left << schedules[tindex][i][0] << setw(20) << left << schedules[tindex][i][1] << endl;
+    }
+
+    separator();
 }
